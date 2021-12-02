ed845b869953b6bd98d3058e965243433630a871
damaser5 <91973072+damaser5@users.noreply.github.com>
2021-12-02 21:28:52 +0100

    Update PrincipalService.js

--- service/PrincipalService.js
+++ service/PrincipalService.js
@@ -4,18 +4,18 @@
 /**
  * Permite lanzar la alarma correspondiente con los parámetros de entrada
  *
- * idUsuario BigDecimal El id del usuario
+ * idAlarma BigDecimal El id de la Alarma
  * idAlerta BigDecimal El id de la Alerta
  * returns List
  **/
-exports.alarmaIdUsuarioIdAlertaPOST = function(idUsuario,idAlerta) {
+exports.alarmaIdAlarmaIdAlertaPOST = function(idAlarma,idAlerta) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = [ {
-  "idUsuario" : 1,
+  "idAlarma" : 1,
   "idAlerta" : 1
 }, {
-  "idUsuario" : 1,
+  "idAlarma" : 1,
   "idAlerta" : 1
 } ];
     if (Object.keys(examples).length > 0) {

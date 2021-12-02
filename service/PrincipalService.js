f65c00a82621682a6a0733d2984140c4d4b59f3d
damaser5 <91973072+damaser5@users.noreply.github.com>
2021-12-02 21:31:49 +0100

    Update export

--- service/PrincipalService.js
+++ service/PrincipalService.js
@@ -4,18 +4,18 @@
 /**
  * Permite lanzar la alarma correspondiente con los parámetros de entrada
  *
- * idAlarma BigDecimal El id de la Alarma
+ * idUsuario BigDecimal El id del usuario
  * idAlerta BigDecimal El id de la Alerta
  * returns List
  **/
-exports.alarmaIdAlarmaIdAlertaPOST = function(idAlarma,idAlerta) {
+exports.alarmaIdUsuarioIdAlertaPOST = function(idUsuario,idAlerta) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = [ {
-  "idAlarma" : 1,
+  "idUsuario" : 1,
   "idAlerta" : 1
 }, {
-  "idAlarma" : 1,
+  "idUsuario" : 1,
   "idAlerta" : 1
 } ];
     if (Object.keys(examples).length > 0) {
